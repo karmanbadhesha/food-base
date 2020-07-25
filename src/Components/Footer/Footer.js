dbb5e2a116a6553b505ce2a3ddaf1d42526bd85c
Karman Badhesha <karman.badhesha@edu.sait.ca>
2020-07-25 17:10:29 -0600

    Update url

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
     return (
         <div>
             <p>Created by Karman Badhesha for ShyftBase - July 25, 2020 </p>
-            <p><a href="https://github.com/karmanbadhesha/FoodBase">Github</a></p>
+            <p><a href="https://github.com/karmanbadhesha/food-base">Github</a></p>
         </div >
     );
 
